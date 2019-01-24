21ba9fe807ccf22ea1ad7571e35b95502687f19f
Norbertlm <noobertlm@gmail.com>
2019-01-24 10:37:08 -0500

    clean up seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'rest-client'
 require 'dotenv/load'
 
 def movie_title_giver(num, page)
-  response_string = RestClient.get(ENV['URL'].to_s + "#{page}&with_genres=#{num}")
+  response_string = RestClient.get("#{ENV['URL']}#{page}&with_genres=#{num}")
   response_hash = JSON.parse(response_string)
   response_hash["results"].map{ |movieObj|
      {movieObj["title"] => movieObj["overview"]}
